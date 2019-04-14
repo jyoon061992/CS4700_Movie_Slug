531a1d91b7d5a8dcfb830d32c9a1a516eeac03b0
Wilson Hon <wilsongip@gmail.com>
2019-04-13 18:29:25 -0700

    Changed NPC collision layer so it does not collide with Player

--- Scripts/Player/NPC.gd
+++ Scripts/Player/NPC.gd
@@ -5,7 +5,7 @@ const SPEED = 30
 const FLOOR = Vector2(0, -1)
 
 var velocity = Vector2()
-var direction = 1
+var direction = Global.direction.moveRight
 
 func _ready():
 	pass # Replace with function body.
@@ -16,8 +16,12 @@ func _physics_process(delta):
 	
 	velocity.y += GRAVITY
 	
+	$AnimatedSprite.flip_h = !direction == Global.direction.moveRight
+	
+	$AnimatedSprite.play("walk")
+	
 	velocity = move_and_slide(velocity, FLOOR)
 	
 	if is_on_wall() or !$RayCast2D.is_colliding():
 		direction = -direction
-		$RayCast2D.position *= -1
+		$RayCast2D.position.x *= -1
