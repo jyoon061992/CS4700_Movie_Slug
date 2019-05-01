484a5a4d62a4ca61931ebfe985705d36812f58fb
Walt <wpniederer@cpp.edu>
2019-04-30 22:44:08 -0700

    bombs explode

--- Scripts/Items/Bomb.gd
+++ Scripts/Items/Bomb.gd
@@ -1,13 +1,23 @@
 extends RigidBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var explosionScene = preload("res://Scenes/Etc/Explosion.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Timer_timeout():
+	explode()
+
+func explode():
+	var explosion = explosionScene.instance()
+	get_parent().add_child(explosion)
+	self.queue_free()
+	explosion.position = self.position
+	explosion
