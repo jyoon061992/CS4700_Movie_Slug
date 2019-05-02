56a7ae3e168c7a746609f7ab9fa3a32d5bf24759
Matthew Ortiz <Mattheww.Ortiz@gmail.com>
2019-05-01 22:58:51 -0700

    added supplies.tscn

---
+++ Scripts/Items/supplies.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+export var amount = 20
+var type = "coin"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_Area2D_body_entered(body):
+	if "player" in body.name:
+		body.reload(amount)
+		queue_free()
