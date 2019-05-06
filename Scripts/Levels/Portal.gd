f144d79562e37daf1a384810ca8db5655f2428c2
Wilson Hon <wilsongip@gmail.com>
2019-05-05 23:19:01 -0700

    Removed redundant portal scenes and have a single portal that with an export variable to change to different scenes

--- Scripts/Levels/Portal.gd
+++ Scripts/Levels/Portal.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+export var level_to_teleport_to : String = "TestingGround"
+
 var inBody: bool = false
 
 func _ready():
@@ -7,16 +9,15 @@ func _ready():
 
 func _input(event):
 	if inBody and Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene("res://Scenes/Levels/HubWorld.tscn")
+		get_tree().change_scene("res://Scenes/Levels/"+level_to_teleport_to+".tscn")
 	
-func _on_PortalHome_body_entered(body):
-	print(body.name)
+func _on_Portal_body_entered(body):
 	if "player" in body.name:
 		inBody = true
 	pass 
 
 
-func _on_PortalHome_body_exited(body):
+func _on_Portal_body_exited(body):
 	if "player" in body.name:
 		inBody = false
 	pass
